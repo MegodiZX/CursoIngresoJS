e239bdb6551e9e78f38560b09279405375efcc95
MegodiZX <60672580+MegodiZX@users.noreply.github.com>
2020-02-07 18:59:00 -0300

    edits

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
+	var precio;
+	var porcentajeDeDecuento;
+	var resultado;
+
+	precio= prompt ("Precio");
+	porcentajeDeDecuento= prompt ("Descuento");
+
+
+
+	resultado = precio -precio *porcentajeDeDecuento/100;
+
+	document.getElementById('elPrecioFinal').value=resultado;
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho= prompt ("ingrese ancho");
+	ancho= parseInt (ancho);
+	largo= prompt ("ingrese largo");
+	largo= parseInt (largo);
+
+	perimetro= 2* (ancho + largo);
+
+	alert ("El prerimetro es "+perimetro);
+
+
 
 }
